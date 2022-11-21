bc02c588e558deb54b155edbe225fc2ed78de344
Obinna Bryan <royalobi@yahoo.com>
2022-11-21 22:13:50 +0000

    user management

---
+++ usermgt.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This script will be used to add new users and grant them password
+#Author: Obinna Bryan.
+#Date: 21/11/2022.
+echo "Welcome to the new user management platform"
+echo "Step 1"
+echo "Please enter your preferred username"
+read username
+echo "Processing...."
+sudo useradd $username
+echo "$username, you are welcome!"
+echo "Step 2"
+echo "$username, please enter your preferred password"
+sudo passwd $username
+echo "$username, your preferred password is saved successfully"
+echo "Congratulations, you are welcome!"
